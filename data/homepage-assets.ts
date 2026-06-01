39d722c2c428a7268060f8ad26e20a1026c03106
LongLeo <141803367+LongLeo287@users.noreply.github.com>
2026-06-01 10:43:59 +0700

    feat: add homepage asset data

---
+++ data/homepage-assets.ts
@@ -0,0 +1,27 @@
+export const clientLogos = [
+  { name: "Decordi", src: "/images/legacy/clients/decordi.png" },
+  { name: "Kim Quốc Tiến", src: "/images/legacy/clients/kimquoctien.png" },
+  { name: "Tân Nhất Hương", src: "/images/legacy/clients/tannhathuong.png" },
+  { name: "Thiên Sơn", src: "/images/legacy/clients/thienson.png" },
+  { name: "POS365", src: "/images/legacy/clients/pos365.png" },
+  { name: "TSCHEM", src: "/images/legacy/clients/tschem.png" },
+  { name: "Vietmap", src: "/images/legacy/clients/vietmap.png" },
+  { name: "MISA", src: "/images/legacy/clients/misa.png" },
+  { name: "Mạnh Hệ", src: "/images/legacy/clients/manhhe.png" },
+  { name: "An Thiên Hương", src: "/images/legacy/clients/anthienhuong.png" }
+];
+
+export const pressLogos = [
+  { name: "CafeF", src: "/images/legacy/press/cafef.png" },
+  { name: "Dân trí", src: "/images/legacy/press/dantri.png" },
+  { name: "Thanh Niên", src: "/images/legacy/press/thanhnien.png" },
+  { name: "Vietnamnet", src: "/images/legacy/press/vietnamnet.png" },
+  { name: "Zing News", src: "/images/legacy/press/zing-news.png" }
+];
+
+export const featuredPosts = [
+  { title: "SEO là gì?", category: "SEO cơ bản", href: "/seo-la-gi/", image: "/images/legacy/blog/seo-la-gi.jpg" },
+  { title: "Nghiên cứu từ khóa", category: "Keyword Research", href: "/seo/keyword-research/", image: "/images/legacy/blog/nghien-cuu-tu-khoa.jpg" },
+  { title: "SEO Onpage", category: "SEO Onpage", href: "/seo/onpage/", image: "/images/legacy/blog/seo-onpage.jpg" },
+  { title: "Technical SEO", category: "SEO Technical", href: "/seo/technical/", image: "/images/legacy/blog/technical-seo.jpg" }
+];
